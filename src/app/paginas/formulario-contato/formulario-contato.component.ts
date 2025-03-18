68973e2a40b19462184bd817fd62d96605b15a05
saeleuterio <saeleuterio@hotmail.com>
2025-03-18 20:01:05 -0300

    up

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from "../../componentes/container/container.component";
 import { SeparadorComponent } from "../../componentes/separador/separador.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
@@ -10,11 +11,33 @@ import { SeparadorComponent } from "../../componentes/separador/separador.compon
     CommonModule,
     ContainerComponent,
     SeparadorComponent,
-    SeparadorComponent
+    ReactiveFormsModule
 ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
 export class FormularioContatoComponent {
 
+  contatoForm!: FormGroup;
+
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl('')
+    })
+  }
+
+  salvarContato() {
+    if(this.contatoForm.valid) {
+      console.log(this.contatoForm.value);
+    }
+  }
+
+  cancelar() {
+    console.log('Submissao cancelada')
+  }
 }
