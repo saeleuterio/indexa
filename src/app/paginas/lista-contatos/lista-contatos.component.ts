893fa26d26bc94d8e9681a75d4336531a07a98e6
saeleuterio <saeleuterio@hotmail.com>
2025-03-20 08:30:50 -0300

    up

--- src/app/paginas/lista-contatos/lista-contatos.component.ts
+++ src/app/paginas/lista-contatos/lista-contatos.component.ts
@@ -14,6 +14,7 @@ interface Contato {
 }
 
 import agenda from '../../agenda.json'
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-lista-contatos',
@@ -25,7 +26,8 @@ import agenda from '../../agenda.json'
     SeparadorComponent,
     ContatoComponent,
     FormsModule,
-    FormularioContatoComponent
+    FormularioContatoComponent,
+    RouterLink
   ],
   templateUrl: './lista-contatos.component.html',
   styleUrl: './lista-contatos.component.css'
